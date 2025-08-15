5f865273cb140f0246ea1de3f059ba0cd80740fc
Eli Sandoval <eli.sandoval@uberall.com>
2025-08-15 18:17:17 -0300

    Added db logger method

---
+++ lib/request_logger.rb
@@ -0,0 +1,18 @@
+require_relative 'database'
+require_relative 'app_config'
+
+class RequestLogger
+  INSERT_SQL = <<-SQL
+    INSERT INTO request_logs(url, name_parameter, response_status, response_text)
+    VALUES (?, ?, ?, ?)
+  SQL
+
+  # Logs a single API request to the database
+  def self.log(url, name_parameter, status, response)
+    db = Database.instance
+    db.execute(INSERT_SQL, [url, name_parameter, status, response])
+
+  rescue StandardError => e
+    puts "Failed to log request: #{e.message}"
+  end
+end
